d5b907e8d1ee0155ca2aa4caaf5c2b440f30ce36
Franco Galbiati <francogalbiati984@gmail.com>
2025-03-03 11:52:35 -0300

    add error

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import * as cors from 'cors';
+import { GlobalExceptionFilter } from './erroes';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn', 'debug', 'verbose'],
   });
 
-
-
   app.setGlobalPrefix('api');
   app.use(cors({
     origin: '*',
-    methods: ['GET', 'POST',],
+    methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   }));
   app.useGlobalPipes(
@@ -23,10 +22,13 @@ async function bootstrap() {
     })
   );
 
+  // Registrar el filtro global de excepciones
+  app.useGlobalFilters(new GlobalExceptionFilter());
 
   await app.listen(process.env.PORT ?? 3001, () => {
     console.log(`Server is running on port ${process.env.PORT ?? 3001}`);
   });
+
   const logger = new Logger('Payments-ms');
   logger.log(`App running on port ${process.env.PORT ?? 3001}`);
 }
